4f68cea5d3e05fdd16309b5718d7c023045e22bd
Ayesha Bhatti <abhatt13@mail.bbk.ac.uk>
2022-05-30 21:10:11 +0100

    Fibonacci solution using recursion

---
+++ src/Recursion/Fibonacci/Fibonacci.java
@@ -0,0 +1,13 @@
+package Recursion.Fibonacci;
+
+public class Fibonacci {
+
+    public static int fib (int n) {
+        return switch (n) {
+            case 0 -> 0;
+            case 1 -> 1;
+            default -> fib(n - 1) + fib(n - 2);
+        };
+    }
+
+}
