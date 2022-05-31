2b8dbb7b0a67e94f9a71878e2936346105543ff2
Ayesha Bhatti <abhatt13@mail.bbk.ac.uk>
2022-05-31 10:36:47 +0100

    Optionals in Stack

--- src/DataStructures/Stack/StackFactory.java
+++ src/DataStructures/Stack/StackFactory.java
@@ -7,6 +7,12 @@ public class StackFactory<T> {
 
     public Stack<T> factoryMethod(String stackType) {
 
+        /**
+         * Alternative optional
+         * Optional<String> optionalValue = wordList.stream().filter(s -> s.contains("fred")).findFirst();
+         * System.out.print(optionalValue.orElse("No word") + " contains fred");
+         */
+
         String str = Optional.ofNullable(stackType).orElseGet(() -> "Stack"); // default implementation of Stack if null value
 
         if (str.equals("Stack")) {
